de1d5584691312d41510a8507a92304d095c14ee
tharvey <tharveyster@gmail.com>
2021-10-06 15:45:46 -0400

    Moved styling from resume file to css file

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,21 +2,13 @@ import React from "react";
 import myResume from '../../todd-harvey-resume.pdf'
 
 export default function Resume() {
-  const skills = {
-    backgroundColor: '#cccccc',
-    margin: 0,
-    padding: 20
-  }
-  const skillList = {
-    listStyle: 'none'
-  }
   return (
-    <div style={skills}>
+    <div className="skills">
       <h2>Resume</h2>
       <p>My resume can be <a className="resume-link" href={myResume} rel="noreferrer" target="_blank">downloaded here</a>.</p>
 
       <h3>Frontend Proficiencies</h3>
-      <ul style={skillList}>
+      <ul className="skillList">
         <li>HTML</li>
         <li>CSS</li>
         <li>JavaScript</li>
@@ -26,7 +18,7 @@ export default function Resume() {
       </ul>
 
       <h3>Backend Proficiencies</h3>
-      <ul style={skillList}>
+      <ul className="skillList">
         <li>Node</li>
         <li>Express</li>
         <li>MySQL, Sequelize</li>
