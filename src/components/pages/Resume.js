e284bdf90b9cde57e454020b9c4f63ed8fbd4067
tharvey <tharveyster@gmail.com>
2021-10-12 12:49:27 -0400

    Attempting to resolve an issue with the resume not updating

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,11 +1,9 @@
 import React from "react";
-import myResume from '../../todd-harvey-resume.pdf'
 
 export default function Resume() {
   return (
     <div className="skills">
       <h2>Resume</h2>
-      <p>My resume can be <a className="resume-link" href={myResume} rel="noreferrer" target="_blank">downloaded here</a>.</p>
 
       <h3>Frontend Proficiencies</h3>
       <ul className="skillList">
